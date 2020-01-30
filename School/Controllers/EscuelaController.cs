269397ecdacc40d4aa1941264f690a74f561b897
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-01-29 19:24:18 -0600

    Primer Commit -Done

---
+++ School/Controllers/EscuelaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using School.Models;
+
+namespace School.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        // GET: Escuela
+        public ActionResult Tabla()
+        {
+            using (SchoolEntities oTabla = new SchoolEntities())
+            {
+                var lTabla = from s in oTabla.School_Table select s;
+
+                return View(lTabla.ToList());
+            }            
+        }
+    }
+}
